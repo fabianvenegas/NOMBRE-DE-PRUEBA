81b9f17176c70190bf9fefc198a3b77b368bc01a
benjobas <166581907+benjobas@users.noreply.github.com>
2024-09-11 11:51:03 -0300

    se arregla el form

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-form.addEventListener('click', function() {
-  let formElement = document.getElementById('form');
-formElement.scrollIntoView({ behavior: 'smooth' });
-});
-  function presionarBoton() {
-    // capturar los valores de los inputs
-    let nombre = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let mensaje = document.getElementById('message').value;
-    // enviar los valores en un objeto a la consola
-  
-    if(nombre === '' || email === '' || mensaje === '') {
-      alert('Por favor, complete todos los campos');
-      return;
-    }
-    // mostrar un mensaje de alerta
-    alert('Gracias por enviar tu mensaje');
-    console.log({nombre, email, mensaje});
-    // limpiar los inputs
-    document.getElementById('name').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('message').value = '';
+
+let form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  let nombre = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let mensaje = document.getElementById('message').value;
+
+  if (nombre === '' || email === '' || mensaje === '') {
+    alert('Por favor, complete todos los campos');
+    return;
   }
+
+  alert('Gracias por enviar tu mensaje');
+
+  console.log({ nombre, email, mensaje });
+
+  form.reset();
+});
+
+document.getElementById('form').scrollIntoView({ behavior: 'smooth' });
